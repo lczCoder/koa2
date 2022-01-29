192c97dc0eb1ac80644169c840c6ec90fcab0b1d
lczCoder <lczcoder@163.com>
2022-01-29 22:17:34 +0800

    feat:路由重定向

--- router/index.js
+++ router/index.js
@@ -1,10 +1,14 @@
 // koa2 路由配置入口文件
-const Router = require('koa-router')
-const people = require('./people')
+const Router = require("koa-router");
+const people = require("./people");
 
 // 创建路由表
-const router = new Router()
+const router = new Router();
 
-router.use('/people',people.routes(),people.allowedMethods())
+// 路由模块
+router.use("/people", people.routes(), people.allowedMethods());
 
-module.exports = router
+// 路由重定向
+router.redirect("/", "/people");
+
+module.exports = router;
--- router/people.js
+++ router/people.js
@@ -1,18 +1,17 @@
-const Router = require('koa-router')
+const Router = require("koa-router");
 
-const people = new Router()
+const people = new Router();
 
-people.get('/',async (ctx)=>{
-  ctx.body = '人类'
-})
+people.get("/", async (ctx) => {
+  ctx.body = "人类";
+});
 
-people.get('/boy',async (ctx)=>{
-  ctx.body = '男孩'
-})
+people.get("/boy", async (ctx) => {
+  ctx.body = "男孩";
+});
 
-people.get('/girl',async (ctx)=>{
-  ctx.body = '女孩'
-})
+people.get("/girl", async (ctx) => {
+  ctx.body = "女孩";
+});
 
-
-module.exports = people
+module.exports = people;
