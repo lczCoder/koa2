70761823064194536f7ad3d6011f99bb63766323
lczCoder <lczcoder@163.com>
2022-01-29 22:13:11 +0800

    feat:路由模块拆分

--- router/index.js
+++ router/index.js
@@ -1,15 +1,10 @@
 // koa2 路由配置入口文件
 const Router = require('koa-router')
+const people = require('./people')
 
 // 创建路由表
 const router = new Router()
 
-router.get('/',async (ctx)=>{
-  ctx.body = '首页'
-})
-
-router.get('/list',async (ctx)=>{
-  ctx.body = '列表页面'
-})
+router.use('/people',people.routes(),people.allowedMethods())
 
 module.exports = router
---
+++ router/people.js
@@ -0,0 +1,18 @@
+const Router = require('koa-router')
+
+const people = new Router()
+
+people.get('/',async (ctx)=>{
+  ctx.body = '人类'
+})
+
+people.get('/boy',async (ctx)=>{
+  ctx.body = '男孩'
+})
+
+people.get('/girl',async (ctx)=>{
+  ctx.body = '女孩'
+})
+
+
+module.exports = people
