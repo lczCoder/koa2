4a500719dc93b0a9e2452da134127f13346dc17e
lczCoder <lczcoder@163.com>
2022-02-03 21:53:19 +0800

    feat:处理fromdata格式数据

--- app.js
+++ app.js
@@ -1,23 +1,21 @@
 // koa2 项目的入口文件
-const Koa = require('koa2')
-const router = require('./router')
-const cors = require('koa2-cors')
-const koaBody = require('koa-body')
+const Koa = require("koa2");
+const router = require("./router");
+const cors = require("koa2-cors");
+const koaBody = require("koa-body");
 
 // 创建项目实例
-const app = new Koa()
+const app = new Koa();
 
 // 解决跨域问题
 app.use(cors());
 // 解析post请求参数
-app.use(koaBody({
-
-}))
+app.use(koaBody({ multipart: true }));
 
 // 使用中间件
-app.use(router.routes(),router.allowedMethods())
+app.use(router.routes(), router.allowedMethods());
 //router.allowedMethods() 允许接收任何的请求类型 get post put ……
 
-app.listen(5050,()=>{
-  console.log('koa2 服务启动成功!');
-})
+app.listen(5050, () => {
+  console.log("koa2 服务启动成功!");
+});
--- router/people.js
+++ router/people.js
@@ -5,13 +5,14 @@ const people = new Router();
 
 people.post("/", async (ctx) => {});
 
+// 处理json默认数据
 people.post("/boy", async (ctx) => {
   let sql = "INSERT INTO info (name,age) VALUES(?,?)";
   let sqlParams = ["zz", "24"];
   db.query(sql, sqlParams, (error, results) => {
     if (error) throw error;
     console.log("数据库插入成功", results);
-    db.end()
+    db.end();
   });
 
   const { name, address } = ctx.request.body;
@@ -21,8 +22,12 @@ people.post("/boy", async (ctx) => {
   };
 });
 
-people.get("/girl", async (ctx) => {
-  ctx.body = "女孩";
+// 处理formdata格式的数据
+people.post("/girl", async (ctx) => {
+  const { age } = ctx.request.body;
+  ctx.body = {
+    age
+  };
 });
 
 module.exports = people;
